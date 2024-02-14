9e81cc0b7bd89318d2d092a6ad35eae05003295a
coderrehad <engr.rehad@gmail.com>
2024-02-14 17:35:30 +0600

    30-35 class has been dome

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,10 @@ import Link from "next/link";
 const Page = () => {
     return (
         <>
-            <h1>Home page</h1>
-            <h1><Link href='/product'>Product</Link></h1><br/>
-            <Link href='/posts'>Posts</Link>
+            <h1>Home page</h1><br/><br/>
+            <Link className='btn btn-primary' href='/product'>Product</Link><br/><br/>
+            <Link className='btn btn-primary' href='/posts'>Posts</Link><br/><br/>
+            <Link className='btn btn-primary' href='/comments'>Comments</Link><br/><br/>
         </>
     );
 };
